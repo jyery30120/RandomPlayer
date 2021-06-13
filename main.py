de4f6aa23e011bb287ad386efedc87463c1f2ffc
jyery30120 <jyery30120@gmail.com>
2021-06-14 00:20:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import random
+# 引入 tkinter 模組
+import tkinter as tk
+
+def OpenVideo():
+	Path = PathString.get()
+	List = os.listdir(Path)
+	r = random.randint(0, len(List))
+	fName = List[r]
+
+	if fName[-3:].lower() in ('mp4','avi','flv','rmbv','wmv'):
+		os.system( Path + '\\' + List[r])
+	else:
+		print('非影片')	
+		
+
+window = tk.Tk()
+window.title('Hello World')
+window.geometry("300x100+250+150")
+
+labelPath = tk.Label(window,
+                    text = "Path")
+labelPath.pack()
+PathString = tk.Entry(window)
+PathString.pack()
+
+button = tk.Button(window,         
+                   text = '隨機撥放',  
+				   command = OpenVideo)
+#button.pack()
+button.pack()  
+window.mainloop()
